1e9daebba1f3f3c6dfa0114f6a04e5d2b16ab7f9
Dylan Jennings <djennings@sailthru.com>
2019-10-11 10:05:38 +1300

    Update API to be more user friendly
    Having constructors with multiple arguments with the same type where
    ordering is significant is not a good pattern...

--- errors.go
+++ errors.go
@@ -1,39 +1,85 @@
-package errors
+package friendly
 
 import (
 	"errors"
 )
 
-var DefaultUserError = errors.New("Something went wrong.")
+var DefaultUserError = errors.New("Something went wrong")
 
 type Error struct {
-	error     error
-	userError error
+	cause    error
+	friendly error
+}
+
+func New() Error {
+	return Error{
+		cause:    DefaultUserError,
+		friendly: DefaultUserError,
+	}
+}
+
+// WithCause sets the internal non-user-safe cause of the error.
+func (e Error) WithCause(err error) Error {
+	e.cause = err
+	return e
+}
+
+// WithFriendly sets the user-safe cause of the error.
+func (e Error) WithFriendly(err error) Error {
+	e.friendly = err
+	return e
+}
+
+// WithCauseString sets the internal non-user-safe cause of the error.
+func (e Error) WithCauseString(err string) Error {
+	e.cause = errors.New(err)
+	return e
+}
+
+// WithFriendlyString sets the user-safe cause of the error.
+func (e Error) WithFriendlyString(err string) Error {
+	e.friendly = errors.New(err)
+	return e
+}
+
+// Err returns the friendly error as an 'error', if an underlying cause is
+// present.
+func (e Error) Err() error {
+	if e.cause == nil {
+		return nil
+	}
+	return e
+}
+
+// Cause returns the underlying cause error.
+func (e Error) Cause() error {
+	return e.cause
+}
+
+// Friendly returns the underlying friendly error.
+func (e Error) Friendly() error {
+	return e.friendly
 }
 
 func (e Error) Error() string {
-	return e.error.Error()
+	return e.cause.Error()
 }
 
-func (e Error) UserError() error {
-	return e.userError
-}
-
-// Cause takes any error and will return the first user-friendly error it finds
+// Friendly takes any error and will return the first user-friendly error it finds
 // as it traverses up through the linked list. If there are no user-friendly
 // causes found, nil is returned.
-func User(err error) error {
+func Friendly(err error) error {
 	type errCauser interface {
 		Cause() error
 	}
-	type errUser interface {
-		UserError() error
+	type errFriendlyer interface {
+		Friendly() error
 	}
 
 	for err != nil {
-		user, ok := err.(errUser)
+		user, ok := err.(errFriendlyer)
 		if ok {
-			return user.UserError()
+			return user.Friendly()
 		}
 
 		cause, ok := err.(errCauser)
@@ -44,30 +90,4 @@ func User(err error) error {
 	}
 
 	return nil
-}
-
-func NewError(err, user error) error {
-	if err == nil {
-		return nil
-	}
-
-	if user == nil {
-		user = DefaultUserError
-	}
-
-	return &Error{
-		error:     err,
-		userError: user,
-	}
-}
-
-func NewErrorString(err, user string) error {
-	if err == "" {
-		return nil
-	}
-
-	return &Error{
-		error:     errors.New(err),
-		userError: errors.New(user),
-	}
 }
